f4c8747dcd0de7053bed9ab2673b79197c1ca968
José Lucas Torquato <jltorquato12@gmail.com>
2023-07-24 09:00:09 -0300

    feat: add table to integration for active-camapign

--- integrations/views.py
+++ integrations/views.py
@@ -4,21 +4,15 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-
-def integration_view(request):
-    return render(request, 'pages/integrations/integration.html')
-
 class WebhookActiveCampaign(APIView):
     def post(self, request, id):
         try:
-            print(id)
-            print(request.data.dict())
             data = self._process_data(request.data.dict())
-            print("DATA =>", data)
             phone_number = data.get("phone", "")
             formatted_phone = self._format_phone_number(phone_number)
             formatted_message = f'{data.get("list", "")}:\nNome: {data.get("first_name", "")}\nContato: {data.get("phone", "")}\nhttps://wa.me/+55{formatted_phone}'
 
+            # Enviar para a fila do whatsapp
             print("FORMAT", formatted_message)
             return Response(formatted_message, status=status.HTTP_200_OK)
         except Exception as e:
@@ -38,7 +32,17 @@ class WebhookActiveCampaign(APIView):
         }
 
     def _format_phone_number(self, phone_number):
-        print("FORMART")
         cleaned_phone = phone_number.replace("(", "").replace(")", "").replace(" ", "").replace("-", "").replace("+55", "")
         return cleaned_phone
 
+
+def integration_view(request):
+    webhook_context = { 
+        "status": "Desativado",
+        "url": "-",#f"http://15.228.146.110/integrations/{request.user.id}/active-campaign",
+        "countCalls": 0
+    }
+    return render(
+        request, 
+        'pages/integrations/integration.html', 
+        context={ "weebhook": webhook_context })
