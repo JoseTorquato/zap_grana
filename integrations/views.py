88409dd81087cf781607cb8833f06aa73bf0c9a6
José Lucas Torquato <jltorquato12@gmail.com>
2023-08-06 17:45:34 -0300

    fix: prints guru

--- integrations/views.py
+++ integrations/views.py
@@ -132,9 +132,9 @@ class WebhookGuru(APIView):
 
             return Response(response, status=status.HTTP_200_OK)
         except Exception as e:
-            print(e)
-            print("DATA =>", data)
-            print("USER =>", user)
+            # print(e)
+            # print("DATA =>", data)
+            # print("USER =>", user)
             return Response({ "body": str(e)}, status=status.HTTP_200_OK)
 
     def _process_data(self, data):
