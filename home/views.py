be06cfa7e6d262deb76bbd0dce4aaea3e75dd2fe
José Lucas Torquato <jltorquato12@gmail.com>
2023-08-07 20:20:04 -0300

    feate: create route login and logout

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
-from profile import Profile
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 
 @login_required(login_url='/login')
@@ -9,3 +11,23 @@ def home_view(request):
             return render(request, 'index.html')
     except:    
         return redirect('/profile')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        print(username, password)
+        user = authenticate(username=username, password=password)
+        print(user)
+        if user:
+            login(request, user)
+            return redirect('/')
+        messages.error(request, "Email ou senha inválidos. Por favor, tente novamente.")
+        return redirect('login')
+    return render(request, 'auth/login.html')
+
+@login_required(login_url='/login')
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect('/login')
