f05b52b46b4693cb67e3962498ad5557ad3a4934
José Lucas Torquato <jltorquato12@gmail.com>
2023-08-09 15:24:38 -0300

    feat: create admin view

--- integrations/admin.py
+++ integrations/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Integration
 
-# Register your models here.
+class IntegrationAdmin(admin.ModelAdmin):
+    list_display = ('platform', 'status', 'user_uuid')
+    search_fields = ('platform', 'status', 'user_uuid')
+    fields = ('platform', 'status', 'call_count')
+
+
+admin.site.register(Integration, IntegrationAdmin)
--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'uuid')
+    search_fields = ('name', 'email', 'uuid')
+    fields = ('name', 'email', 'phone')
+
+
+admin.site.register(Profile, ProfileAdmin)
